870df11d29ecf2259b3a0cdb3f1cbc97d6b10978
Kelvin Fichter <kelvinfichter@gmail.com>
2021-07-02 20:16:00 +0200

    put stats in file

--- ops/scripts/stats.sh
+++ ops/scripts/stats.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# set up the stats file
+mkdir ~/logs
+touch ~/logs/stats
+
 while true; do
-  echo "$(date) ----------------";
-  echo "location ---------------";
-  pwd;
-  echo "total memory usage -----";
-  free -m;
-  echo "docker stats -----------";
-  docker stats --no-stream;
-  echo "memory munchers --------";
-  ps aux --sort=-%mem | head;
+  {
+    echo "$(date) ----------------";
+    echo "location ---------------";
+    pwd;
+    echo "total memory usage -----";
+    free -m;
+    echo "docker stats -----------";
+    docker stats --no-stream;
+    echo "memory munchers --------";
+    ps aux --sort=-%mem | head;
+  } >> ~/logs/stats
   sleep 1;
 done
